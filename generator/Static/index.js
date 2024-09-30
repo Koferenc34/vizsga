8a79a1bdb9b16b186c35cc45872dbde4259eac3d
Kovács Ferenc <koferenc34523@gmail.com>
2024-09-30 07:00:40 +0200

    Optimized for mobile

--- generator/Static/index.js
+++ generator/Static/index.js
@@ -4,6 +4,10 @@ const Main = () => {
     document.querySelector('.userDiv').style.display = 'none';
 
     Space1()
+   
+    window.addEventListener('resize', event => {
+        document.querySelector('.space--2').innerHTML = window.innerWidth
+    })
 
 }
 
